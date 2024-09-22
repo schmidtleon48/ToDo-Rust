8513fd4013d8774c3a81babeb016b220a3520a9a
leon <usqho@student.kit.edu>
2024-09-22 16:24:21 +0200

    Final Commit

---
+++ todo_app/target/debug/deps/todo_app-05c285e420625867.d
@@ -0,0 +1,5 @@
+/home/leon/Documents/ToDo-Rust/todo_app/target/debug/deps/libtodo_app-05c285e420625867.rmeta: src/main.rs
+
+/home/leon/Documents/ToDo-Rust/todo_app/target/debug/deps/todo_app-05c285e420625867.d: src/main.rs
+
+src/main.rs:
---
+++ todo_app/target/debug/deps/todo_app-ed3f80164ee933c8.d
@@ -0,0 +1,5 @@
+/home/leon/Documents/ToDo-Rust/todo_app/target/debug/deps/libtodo_app-ed3f80164ee933c8.rmeta: src/main.rs
+
+/home/leon/Documents/ToDo-Rust/todo_app/target/debug/deps/todo_app-ed3f80164ee933c8.d: src/main.rs
+
+src/main.rs:
